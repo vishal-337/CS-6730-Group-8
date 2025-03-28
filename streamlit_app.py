c13bf952a06a52d3b2ed45e0456557157570d8f2
Vishal Maradana <110719396+vishal-337@users.noreply.github.com>
2025-03-28 19:42:59 -0400

    creating streamlit_app

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Set page configuration
+st.set_page_config(
+    page_title="SCI: Social Connectedness Index",
+    page_icon="🌐",
+    layout="wide"
+)
+
+# Main title
+st.title("SCI: Social Connectedness Index") 
