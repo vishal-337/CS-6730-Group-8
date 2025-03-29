04556541efafca495ca780c9c07e81c44e89a321
Vishal Maradana <110719396+vishal-337@users.noreply.github.com>
2025-03-28 20:18:42 -0400

    going back to static

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit.components.v1 as components
+import tableauserverclient as TSC
 
 # Set page configuration
 st.set_page_config(
@@ -11,30 +11,60 @@ st.set_page_config(
 # Main title
 st.title("SCI: Social Connectedness Index")
 
-server_url = st.secrets["tableau"]["server_url"].rstrip("/")
-site_id = st.secrets["tableau"]["site_id"]
-token_name = st.secrets["tableau"]["token_name"]
-token_value = st.secrets["tableau"]["personal_access_token"]
-workbook_name = "SCI"
-view_name = "Dashboard 1"
+# Set up connection to Tableau
+tableau_auth = TSC.PersonalAccessTokenAuth(
+    st.secrets["tableau"]["token_name"],
+    st.secrets["tableau"]["personal_access_token"],
+    st.secrets["tableau"]["site_id"],
+)
+server = TSC.Server(st.secrets["tableau"]["server_url"], use_server_version=True)
 
-# Format for trusted authentication
-# :embed=yes&:toolbar=no adds parameters to avoid login and customize display
-trusted_tableau_url = f"{server_url}/t/{site_id}/views/{workbook_name}/{view_name.replace(' ', '')}?:embed=yes&:toolbar=yes&:tabs=no&:token_type=personal_access_token&:token_name={token_name}&:token_value={token_value}"
+# Specific workbook ID from the URL
+TARGET_WORKBOOK_ID = "2526031"
+TARGET_VIEW_NAME = "Dashboard 1"
 
-# Use iframe for simpler embedding with authentication tokens in URL
-tableau_embed_html = f"""
-<div style='width: 100%; height: 800px;'>
-    <iframe src="{trusted_tableau_url}" 
-            frameborder="0" 
-            width="100%" 
-            height="100%" 
-            allowfullscreen>
-    </iframe>
-</div>
-"""
+# Get various data from Tableau
+@st.cache_data(ttl=600)
+def get_dashboard():
+    with server.auth.sign_in(tableau_auth):
+        workbooks, pagination_item = server.workbooks.get()
+        
+        target_workbook = None
+        for workbook in workbooks:
+            if workbook.id == TARGET_WORKBOOK_ID or workbook.name == "SCI":
+                target_workbook = workbook
+                break
+                
+        if target_workbook:
+            server.workbooks.populate_views(target_workbook)
+            
+            dashboard_view = None
+            for view in target_workbook.views:
+                if view.name == TARGET_VIEW_NAME:
+                    dashboard_view = view
+                    break
+            
+            if dashboard_view:
+                server.views.populate_image(dashboard_view)
+                return dashboard_view.image
+            
+            elif target_workbook.views:
+                view_item = target_workbook.views[0]
+                server.views.populate_image(view_item)
+                return view_item.image
+                
+        return None
 
-components.html(tableau_embed_html, height=800)
+try:
+    dashboard_image = get_dashboard()
+    
+    if dashboard_image:
+        st.image(dashboard_image)
+    else:
+        st.error("Dashboard not found")
+        
+except Exception as e:
+    st.error(f"Error: {str(e)}")
 
 with st.expander("About SCI"):
     st.write("""
