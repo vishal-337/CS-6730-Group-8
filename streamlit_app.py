8fd7a7c0715753399555b1aeb8911089fc3cc853
Vishal Maradana <110719396+vishal-337@users.noreply.github.com>
2025-03-28 21:04:31 -0400

    added covid data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import tableauserverclient as TSC
+import streamlit.components.v1 as components
 
 # Set page configuration
 st.set_page_config(
@@ -65,6 +66,20 @@ try:
         
 except Exception as e:
     st.error(f"Error: {str(e)}")
+
+st.subheader("COVID Data")
+covid_tableau_html = """
+<div class='tableauPlaceholder' id='viz1743210082495' style='position: relative'><noscript><a href='#'><img alt='Daily cases&#47; Month ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;6H&#47;6HTWTQ2C9&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='path' value='shared&#47;6HTWTQ2C9' /> <param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;6H&#47;6HTWTQ2C9&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>
+<script type='text/javascript'>
+    var divElement = document.getElementById('viz1743210082495');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+</script>
+"""
+components.html(covid_tableau_html, height=600)
 
 with st.expander("About SCI"):
     st.write("""
