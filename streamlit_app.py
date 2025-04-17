40691f06f8b135b60aa27d3805116d7310b4e91b
Chennupati, Gadaadhar <gchennupati3@gatech.edu>
2025-04-17 14:10:24 -0400

    COVID State
    State chance

--- streamlit_app.py
+++ streamlit_app.py
@@ -182,45 +182,7 @@ st.markdown("""
 Here in the two graphs we can compare the SCI data for all the US states with the covid data. In the drop down we can change the timings for each month between the period Jan 2020 to Mar 2023. We can also select any of the states to check the SCI connectedness from that state to other states. We use a log value to make sure the smaller SCI score are also visible. The user can change months to see the progress of the COVID cases over time.
 """)
 covid_tableau_html = """
-<div class='tableauPlaceholder' id='viz1744597949361' style='position: relative; margin: 0 auto;'>
-    <noscript>
-        <a href='#'><img alt='Main ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CO&#47;COVIDvsSCI&#47;Main&#47;1_rss.png' style='border: none' /></a>
-    </noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> 
-    <param name='embed_code_version' value='3' /> 
-    <param name='site_root' value='' />
-    <param name='name' value='COVIDvsSCI&#47;Main' />
-    <param name='tabs' value='no' />
-    <param name='toolbar' value='yes' />
-    <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CO&#47;COVIDvsSCI&#47;Main&#47;1.png' /> 
-    <param name='animate_transition' value='yes' />
-    <param name='display_static_image' value='yes' />
-    <param name='display_spinner' value='yes' />
-    <param name='display_overlay' value='yes' />
-    <param name='display_count' value='yes' />
-    <param name='language' value='en-US' />
-    <param name='filter' value='publish=yes' />
-    </object>
-</div>                
-<script type='text/javascript'>                   
-    var divElement = document.getElementById('viz1744597949361');                    
-    var vizElement = divElement.getElementsByTagName('object')[0];                    
-    if ( divElement.offsetWidth > 800 ) 
-    { 
-        vizElement.style.width='1620px';vizElement.style.height='867px';
-    } 
-    else if ( divElement.offsetWidth > 500 ) 
-    { 
-        vizElement.style.width='1620px';
-        vizElement.style.height='867px';
-    } 
-    else 
-    { 
-        vizElement.style.width='100%';vizElement.style.height='877px';
-    }                     
-    var scriptElement = document.createElement('script');                    
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
-</script>
+<div class='tableauPlaceholder' id='viz1744913284938' style='position: relative'><noscript><a href='#'><img alt='Main ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CO&#47;COVIDvsLogSCI&#47;Main&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='COVIDvsLogSCI&#47;Main' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;CO&#47;COVIDvsLogSCI&#47;Main&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1744913284938');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1620px';vizElement.style.height='867px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1620px';vizElement.style.height='867px';} else { vizElement.style.width='100%';vizElement.style.height='877px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
 """
 components.html(covid_tableau_html, height=800)
 
